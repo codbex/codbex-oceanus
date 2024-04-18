6763dda7c7625d9aff16a90adfe5ee721cb8b2cf
MinaDoncheva <mina.doncheva@icloud.com>
2024-04-18 17:07:31 +0300

    remove junit 4 dependency

--- application/src/test/java/com/codbex/oceanus/ui/IntegrationTest.java
+++ application/src/test/java/com/codbex/oceanus/ui/IntegrationTest.java
@@ -12,15 +12,12 @@
 package com.codbex.oceanus.ui;
 
 import com.codbex.oceanus.OceanusApplication;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @AutoConfigureMockMvc
-@RunWith(SpringRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = OceanusApplication.class)
 public abstract class IntegrationTest {
