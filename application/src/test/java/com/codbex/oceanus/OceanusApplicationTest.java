03bd3dc079cc2a3e2e688dd2618b7990c2b00bc0
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-23 11:26:43 +0300

    use specific sftp port to prevent tests failures

--- application/src/test/java/com/codbex/oceanus/OceanusApplicationTest.java
+++ application/src/test/java/com/codbex/oceanus/OceanusApplicationTest.java
@@ -1,9 +1,8 @@
 /*
  * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
@@ -14,12 +13,13 @@ package com.codbex.oceanus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {"DIRIGIBLE_SFTP_PORT=7456"})
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class OceanusApplicationTest {
-	
-	@Test
-	public void contextLoads() {
-	}
+
+    @Test
+    public void contextLoads() {}
 
 }
--- application/src/test/java/com/codbex/oceanus/ui/IntegrationTest.java
+++ application/src/test/java/com/codbex/oceanus/ui/IntegrationTest.java
@@ -1,9 +1,8 @@
 /*
  * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
@@ -16,7 +15,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {"DIRIGIBLE_SFTP_PORT=7654"})
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = OceanusApplication.class)
