305a33315c2ba796dcc8201403ea340a3e16f1cf
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-23 11:09:35 +0200

    Use codbex-platform-parent (#183)
    * Use codbex-platform-parent
    
    * use released parent
    
    * use specific sftp port to prevent tests failures
    
    * use proper way to set DIRIGIBLE_SFTP_PORT

--- application/src/test/java/com/codbex/oceanus/OceanusApplicationTest.java
+++ application/src/test/java/com/codbex/oceanus/OceanusApplicationTest.java
@@ -1,9 +1,8 @@
 /*
  * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
@@ -14,12 +13,16 @@ package com.codbex.oceanus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class OceanusApplicationTest {
-	
-	@Test
-	public void contextLoads() {
-	}
+
+    static {
+        System.setProperty("DIRIGIBLE_SFTP_PORT", "7456");
+    }
+
+    @Test
+    public void contextLoads() {}
 
 }
