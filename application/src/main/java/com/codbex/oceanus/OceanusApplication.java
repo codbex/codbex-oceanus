edd1a001ff49a0f8c236e07fb9fac69fe1c14bb7
delchev <delchevn@gmail.com>
2025-03-02 07:57:13 +0200

    new ui

--- application/src/main/java/com/codbex/oceanus/OceanusApplication.java
+++ application/src/main/java/com/codbex/oceanus/OceanusApplication.java
@@ -16,11 +16,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication(scanBasePackages = {"com.codbex.oceanus", "org.eclipse.dirigible.components"},
         exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
--- application/src/test/java/com/codbex/oceanus/ui/tests/HomePageIT.java
+++ application/src/test/java/com/codbex/oceanus/ui/tests/HomePageIT.java
@@ -17,8 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 class HomePageIT extends UserInterfaceIntegrationTest {
 
-    private static final String CODBEX_HEADER = "codbex";
-
     @Autowired
     private Oceanus oceanus;
 
@@ -26,6 +24,6 @@ class HomePageIT extends UserInterfaceIntegrationTest {
     void testOpenHomepage() {
         oceanus.openHomePage();
 
-        browser.assertElementExistsByTypeAndText(HtmlElementType.HEADER5, CODBEX_HEADER);
+        browser.assertElementExistsByTypeAndText(HtmlElementType.SPAN, "codbex");
     }
 }
