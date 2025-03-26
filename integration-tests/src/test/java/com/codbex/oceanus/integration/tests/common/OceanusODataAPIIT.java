1a0ca0625fcfdd26b8013f4e908ed65a64ef26ae
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:00:17 +0200

    Add more integration tests from dirigible

---
+++ integration-tests/src/test/java/com/codbex/oceanus/integration/tests/common/OceanusODataAPIIT.java
@@ -0,0 +1,6 @@
+package com.codbex.oceanus.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.api.rest.ODataAPIIT;
+
+class KronosODataAPIIT extends ODataAPIIT {
+}
---
+++ integration-tests/src/test/java/com/codbex/oceanus/integration/tests/common/OceanusSecurityIT.java
@@ -0,0 +1,6 @@
+package com.codbex.oceanus.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.api.SecurityIT;
+
+class KronosSecurityIT extends SecurityIT {
+}
