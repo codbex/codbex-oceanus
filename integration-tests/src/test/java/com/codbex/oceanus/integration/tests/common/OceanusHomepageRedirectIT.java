82722c381bd23007227974a72ee955e7a733b6ab
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:23:28 +0200

    fixes

--- integration-tests/src/test/java/com/codbex/oceanus/integration/tests/common/OceanusHomepageRedirectIT.java
+++
@@ -1,6 +0,0 @@
-package com.codbex.oceanus.integration.tests.common;
-
-import org.eclipse.dirigible.integration.tests.ui.tests.HomepageRedirectIT;
-
-class OceanusHomepageRedirectIT extends HomepageRedirectIT {
-}
