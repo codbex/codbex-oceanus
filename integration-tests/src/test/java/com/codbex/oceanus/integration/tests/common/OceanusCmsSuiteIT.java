ceb5c40a3490f78ec313918f5ddfb9e3b43c615b
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:43:38 +0200

    fixes

--- integration-tests/src/test/java/com/codbex/oceanus/integration/tests/common/OceanusCmsSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/oceanus/integration/tests/common/OceanusCmsSuiteIT.java
@@ -1,6 +1,12 @@
 package com.codbex.oceanus.integration.tests.common;
 
+import org.eclipse.dirigible.commons.config.Configuration;
 import org.eclipse.dirigible.integration.tests.api.javascript.cms.CmsSuiteIT;
+import org.eclipse.dirigible.tests.util.PortUtil;
 
 class OceanusCmsSuiteIT extends CmsSuiteIT {
+
+    static {
+        Configuration.set("DIRIGIBLE_SFTP_PORT", Integer.toString(PortUtil.getFreeRandomPort()));
+    }
 }
