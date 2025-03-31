3f8b221732ecb1551ab46b9aea60cad013d7e67a
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:45:52 +0300

    comment the annotations

--- integration-tests/src/test/java/com/codbex/oceanus/integration/tests/DirigibileCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/oceanus/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -1,14 +1,11 @@
 package com.codbex.oceanus.integration.tests;
 
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({//
+//@Suite
+//@SelectClasses({//
 // no tests for now, until https://github.com/eclipse-dirigible/dirigible/pull/4829 is released
 // CmsSuiteIT.class, //
 // SecurityIT.class //
-})
+//})
 public class DirigibileCommonTestSuiteIT {
 
 }
