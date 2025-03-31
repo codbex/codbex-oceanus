d66b579a58bb33fad85cdbbedde1915eb4ccc1eb
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:34:54 +0300

    return old tests

--- integration-tests/src/test/java/com/codbex/oceanus/integration/tests/DirigibileCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/oceanus/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -1,19 +1,14 @@
 package com.codbex.oceanus.integration.tests;
 
-import org.eclipse.dirigible.commons.config.Configuration;
-import org.eclipse.dirigible.tests.util.PortUtil;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({//
-// no tests for now, until https://github.com/eclipse-dirigible/dirigible/pull/4829 is released
-// CmsSuiteIT.class, //
-// SecurityIT.class //
+        // no tests for now, until https://github.com/eclipse-dirigible/dirigible/pull/4829 is released
+        // CmsSuiteIT.class, //
+        // SecurityIT.class //
 })
 public class DirigibileCommonTestSuiteIT {
 
-    static {
-        Configuration.set("DIRIGIBLE_SFTP_PORT", Integer.toString(PortUtil.getFreeRandomPort()));
-    }
 }
