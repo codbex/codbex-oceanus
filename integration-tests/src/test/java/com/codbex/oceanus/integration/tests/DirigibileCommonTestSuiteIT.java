ba87f8264c4ef52a12c7cd725ba1630eb822988e
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:36:14 +0300

    format

--- integration-tests/src/test/java/com/codbex/oceanus/integration/tests/DirigibileCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/oceanus/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -5,9 +5,9 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({//
-        // no tests for now, until https://github.com/eclipse-dirigible/dirigible/pull/4829 is released
-        // CmsSuiteIT.class, //
-        // SecurityIT.class //
+// no tests for now, until https://github.com/eclipse-dirigible/dirigible/pull/4829 is released
+// CmsSuiteIT.class, //
+// SecurityIT.class //
 })
 public class DirigibileCommonTestSuiteIT {
 
